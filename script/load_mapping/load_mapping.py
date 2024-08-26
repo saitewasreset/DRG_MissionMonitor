b7320152aa1bac4cc5a4e38c0c7178445dd4ec52
saitewasreset <saite@buaa.edu.cn>
2024-08-26 10:08:50 +0800

    Add script for loading mission and mapping.

---
+++ script/load_mapping/load_mapping.py
@@ -0,0 +1,108 @@
+import requests
+import json
+
+source_addr = "https://github.com/saitewasreset/DRG_MissionMonitor"
+
+print("Mission Monitor: load_mapping")
+print("made by saitewasreset with love")
+print("Source: {}".format(source_addr))
+
+print()
+print("Afraid of the dark? No need, you got me!")
+print()
+
+print("Loading config.json...")
+
+try:
+    with open("config.json", "r") as f:
+        try:
+            config = json.load(f)
+            admin_endpoint = config["admin_endpoint"]
+            mapping_path = config["mapping_path"]
+        except KeyError as e:
+            print("Invalid config.json: cannot get required key: ", e)
+            input("Press enter to exit...")
+            exit(1)
+        except json.JSONDecodeError as e:
+            print("Invalid config.json: ", e)
+            input("Press enter to exit...")
+            exit(1)
+except OSError as e:
+    print("Cannot open config.json: ", e)
+    input("Press enter to exit...")
+    exit(1)
+
+print("admin endpoint: {}".format(admin_endpoint))
+print("mapping path: {}".format(mapping_path))
+
+def load_mapping(mapping_name: str):
+    mapping = {}
+    print("Loading {}/{}.txt...".format(mapping_path, mapping_name))
+    try:
+        with open("{}/{}.txt".format(mapping_path, mapping_name), "r") as f:
+            for line in f.readlines():
+                line = line.strip()
+
+                if len(line) == 0:
+                    continue
+                if line[0] == "#":
+                    continue
+
+                try:
+                    source, mapped = line.split("|")
+                    mapping[source] = mapped
+                except ValueError:
+                    continue
+    except OSError as e:
+        print("Cannot open {}.txt: ".format(mapping_name), e)
+        input("Press enter to exit...")
+        exit(1)
+    print("Uploading {}...".format(mapping_name))
+    r = requests.post("{}/mapping/{}".format(admin_endpoint, mapping_name), json=mapping)
+    try:
+        res = r.json()
+        print("{}: {}".format(mapping_name, res))
+    except json.JSONDecodeError as e:
+        print("Invalid response from server: ", e)
+        input("Press enter to exit...")
+        exit(1)
+    print("Success")
+
+
+mapping_list = ["character", "entity", "entity_combine", "mission_type", "resource", "weapon", "weapon_combine", "weapon_hero"]
+for mapping_name in mapping_list:
+    load_mapping(mapping_name)
+
+try:
+    with open("{}/entity_blacklist.txt".format(mapping_path), "r") as f:
+        entity_blacklist = [x.strip() for x in f.read().splitlines()]
+        r = requests.post("{}/mapping/entity_blacklist".format(admin_endpoint), json=entity_blacklist)
+        try:
+            res = r.json()
+            print("{}: {}".format("entity_blacklist", res))
+        except json.JSONDecodeError as e:
+            print("Invalid response from server: ", e)
+            input("Press enter to exit...")
+            exit(1)
+except OSError as e:
+    print("Cannot open entity_blacklist.txt: ", e)
+    input("Press enter to exit...")
+    exit(1)
+
+print("Updating cache...")
+r = requests.get("https://rocknstone.top/wearerich/mission_monitor/api/secret20231301_admin/update_essential")
+try:
+    res = r.json()
+    if res["code"] != 200:
+        print("Server returned an error:  ", res)
+        input("Press enter to exit...")
+        exit(1)
+    else:
+        print("Success")
+except json.JSONDecodeError as e:
+    print("Invalid response from server: ", e)
+    input("Press enter to exit...")
+    exit(1)
+
+print("Rock and stone!")
+input("Press enter to exit...")
---
+++ script/load_mission/load_mission.py
@@ -0,0 +1,118 @@
+import requests
+import os
+import re
+import json
+
+source_addr = "https://github.com/saitewasreset/DRG_MissionMonitor"
+
+print("Mission Monitor: load_mission")
+print("made by saitewasreset with love")
+print("Source: {}".format(source_addr))
+
+print()
+print("Afraid of the dark? No need, you got me!")
+print()
+
+print("Loading config.json...")
+
+try:
+    with open("config.json", "r") as f:
+        try:
+            config = json.load(f)
+            admin_endpoint = config["admin_endpoint"]
+            log_path = config["log_path"]
+        except KeyError as e:
+            print("Invalid config.json: cannot get required key: ", e)
+            input("Press enter to exit...")
+            exit(1)
+        except json.JSONDecodeError as e:
+            print("Invalid config.json: ", e)
+            input("Press enter to exit...")
+            exit(1)
+except OSError as e:
+    print("Cannot open config.json: ", e)
+    input("Press enter to exit...")
+    exit(1)
+
+already_uploaded_endpoint = "{}/mission_list".format(admin_endpoint)
+upload_endpoint = "{}/load_mission".format(admin_endpoint)
+update_endpoint = "{}/update_essential".format(admin_endpoint)
+
+print("log path: {}".format(log_path))
+print("already uploaded endpoint: {}".format(already_uploaded_endpoint))
+print("upload endpoint: {}".format(upload_endpoint))
+print("update endpoint: {}".format(update_endpoint))
+
+to_load_list = []
+
+print("Fetching already uploaded data...")
+
+r = requests.get(already_uploaded_endpoint)
+already_uploaded = r.json()["data"]
+
+print("Reading log files...")
+
+for filename in os.listdir(log_path):
+    matched = re.match(".+_([0-9]+).txt", filename)
+    if matched:
+        timestamp_str = matched.group(1)
+        if int(timestamp_str) not in already_uploaded:
+            file_path = "{}/{}".format(log_path, filename)
+            to_load_list.append((int(timestamp_str), file_path))
+
+print("To load mission count: {}".format(len(to_load_list)))
+
+upload_data = []
+
+for timestamp, file_path in sorted(to_load_list, key=lambda x: x[0]):
+    try:
+        with open(file_path, "r", encoding="utf-16-le") as f:
+            timestamp_str = re.search(r"MissionMonitor_([0-9].+)\.txt", file_path).group(1)
+            # skip beginning \ufeff
+            log = f.read()
+            if log[0] == "\ufeff":
+                log = log[1:]
+            upload_data.append((timestamp_str, log))
+    except UnicodeError:
+        print("Cannot decode using utf-16-le: {}".format(file_path))
+        print("Trying utf-8...")
+        with open(file_path, "r", encoding="utf-8") as f:
+            timestamp_str = re.search(r"MissionMonitor_([0-9].+)\.txt", file_path).group(1)
+            # skip beginning \ufeff
+            log = f.read()
+            if log[0] == "\ufeff":
+                log = log[1:]
+            upload_data.append((timestamp_str, log))
+
+print("Uploading...")
+r = requests.post(upload_endpoint, json=upload_data)
+try:
+    res = r.json()
+    if res["code"] != 200:
+        print("Server returned an error:  ", res)
+        input("Press enter to exit...")
+        exit(1)
+    else:
+        print("Success!")
+except json.JSONDecodeError:
+    print("Invalid response from server: ", r.text)
+    input("Press enter to exit...")
+    exit(1)
+
+print("Updating cache...")
+r = requests.get(update_endpoint)
+try:
+    res = r.json()
+    if res["code"] != 200:
+        print("Server returned an error:  ", res)
+        input("Press enter to exit...")
+        exit(1)
+    else:
+        print("Success!")
+except json.JSONDecodeError:
+    print("Invalid response from server: ", r.text)
+    input("Press enter to exit...")
+    exit(1)
+
+print("Rock and stone!")
+input("Press enter to exit...")
