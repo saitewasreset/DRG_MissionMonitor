8ccd870bd70ca05a3265bea32d67448495b7b544
saitewasreset <saite@buaa.edu.cn>
2024-08-28 14:41:07 +0800

    update scripts and README.md(detailed load data instructions).

--- README.md
+++ README.md
@@ -37,6 +37,82 @@ Mission Monitor，《深岩银河》游戏数据分析一站式解决方案。
 
 `$ sudo docker compose up`
 
-### 数据上传
+### 数据初始化与日志文件加载（`./script`）
 
-使用本项目提供的 load_mission、load_mapping 脚本（见 Release）。
+#### 脚本运行环境
+
+数据初始化与日志文件加载脚本使用 Python 3 编写，同时依赖 requests 库。
+
+```shell
+$ pip3 install requests
+```
+
+此外，对于 Windows 系统，也可从[项目 Release](https://github.com/saitewasreset/DRG_MissionMonitor/releases)中下载打包好的版本。
+
+假设部署后后端 API 的根目录为`url = http://127.0.0.1:8080/api`
+
+### `load_hero`
+
+加载游戏中的角色列表。
+
+脚本参数配置：
+
+在`./script/load_hero/config.json`中完成如下配置：
+
+- admin_endpoint：`url/ADMIN_PREFIX`
+
+使用`./script/load_hero/load_hero.py`脚本即可完成加载。
+
+### `load_friends`
+
+加载要重点分析（在伤害统计、KPI 统计中进行展示）的玩家 ID。
+
+脚本参数配置：
+
+在`./script/load_friends/config.json`中完成如下配置：
+
+- admin_endpoint：`url/ADMIN_PREFIX`
+
+在`./script/load_friends/friends.txt`中完成如下配置（该文件需使用 UTF-8 编码，已附带示例）：
+
+- 每行一个游戏 ID
+
+使用`./script/load_friends/load_friends.py`脚本即可完成加载。
+
+### `load_mapping`
+
+为了将游戏内部的武器、敌人、任务 ID 等与中文名称相匹配，需要加载 mapping。
+
+脚本参数配置：
+
+在`./script/load_mapping/config.json`中完成如下配置：
+
+- admin_endpoint：`url/ADMIN_PREFIX`
+- mapping_path：存储 mapping 文件的目录（以附带提供，要获得更新，见[主项目地址](https://github.com/saitewasreset/DRG_MissionMonitor)）
+
+使用`./script/load_mapping/load_mapping.py`脚本即可完成加载。
+
+### `load_kpi`
+
+为了计算玩家 KPI，需要加载 KPI 数据。
+
+脚本参数配置：
+
+在`./script/load_kpi/config.json`中完成如下配置：
+
+- admin_endpoint：`url/ADMIN_PREFIX`
+
+使用`./script/load_kpi/load_kpi.py`脚本即可完成加载。
+
+### `load_mission`
+
+用于将游戏信息加载到后端，游戏日志文件名应为`MissionMonitor_{timestamp}.txt`（由 Mod 自动生成）。
+
+脚本参数配置：
+
+在`./script/load_mission/config.json`中完成如下配置：
+
+- admin_endpoint：`url/ADMIN_PREFIX`
+- log_path：存储游戏日志文件的目录（`./script/load_mission/log`目录中已有测试数据）
+
+使用`./script/load_mission/load_mission.py`脚本即可完成加载。
---
+++ script/load_kpi/load_kpi.py
@@ -0,0 +1,141 @@
+import requests
+import csv
+import sys
+import json
+
+source_addr = "https://github.com/saitewasreset/DRG_MissionMonitor"
+
+print("Mission Monitor: load_kpi")
+print("made by saitewasreset with love")
+print("Source: {}".format(source_addr))
+
+print()
+print("Afraid of the dark? No need, you got me!")
+print()
+
+print("Loading config.json...")
+
+try:
+    with open("config.json", "r", encoding="utf-8") as f:
+        try:
+            config = json.load(f)
+            admin_endpoint = config["admin_endpoint"]
+        except KeyError as e:
+            print("Invalid config.json: cannot get required key: ", e)
+            input("Press enter to exit...")
+            sys.exit(1)
+        except json.JSONDecodeError as e:
+            print("Invalid config.json: ", e)
+            input("Press enter to exit...")
+            sys.exit(1)
+except OSError as e:
+    print("Cannot open config.json: ", e)
+    input("Press enter to exit...")
+    sys.exit(1)
+
+print("admin endpoint: {}".format(admin_endpoint))
+
+character_id_to_game_id = ["NONE", "DRILLER", "ENGINEER", "GUNNER", "SCOUT"]
+
+result = {
+    "version": "0.2.2",
+    "priorityTable": {},
+    "character": {}
+}
+
+try:
+    with open("kpi_data.txt", "r") as f:
+        for line in [x.strip() for x in f.readlines()]:
+            if len(line) == 0:
+                continue
+            if line[0] == "#":
+                continue
+
+            line_data = line.split(" ")
+
+            character_game_id = character_id_to_game_id[int(line_data[0])]
+            character_subtype_id = int(line_data[1])
+            character_subtype_name = line_data[2]
+            character_weight_list = [float(x) for x in line_data[3:]]
+
+            result["character"].setdefault(character_game_id, {})[character_subtype_id] = {
+                "subtypeName": character_subtype_name,
+                "weightList": character_weight_list,
+                "priorityTable": {
+                    "default": 1.0
+                }
+            }
+except Exception as e:
+    print("Cannot parsing kpi_data.txt: ", e)
+    input("Press enter to exit...")
+    sys.exit(1)
+
+try:
+    with open("entity_list_combined.csv", "r") as f:
+        reader = csv.reader(f)
+        reader_iter = reader.__iter__()
+        # skip table header
+        reader_iter.__next__()
+
+        result["priorityTable"]["default"] = 0.0
+
+        for row in reader_iter:
+            entity_game_id = row[0]
+            priority_weight = float(row[2])
+            character_weight_list = [float(x) for x in row[3:]]
+
+            if float(priority_weight) != 0.0:
+                result["priorityTable"][entity_game_id] = float(priority_weight)
+
+            i_map = [("DRILLER", 1), ("GUNNER", 1), ("ENGINEER", 1), ("SCOUT", 1), ("SCOUT", 2)]
+            for i, character_weight in enumerate(character_weight_list):
+                if character_weight != 1.0:
+                    result["character"][i_map[i][0]][i_map[i][1]]["priorityTable"][entity_game_id] = character_weight
+except Exception as e:
+    print("Cannot parsing entity_list_combined.csv: ", e)
+    input("Press enter to exit...")
+    sys.exit(1)
+
+kpi_endpoint = "{}/kpi".format(admin_endpoint)
+update_endpoint = "{}/update_essential".format(admin_endpoint)
+
+print("kpi endpoint: {}".format(kpi_endpoint))
+print("update endpoint: {}".format(update_endpoint))
+
+print("Uploading kpi...")
+try:
+    r = requests.post(kpi_endpoint, json=result)
+except requests.exceptions.RequestException as e:
+    print("Request failed: ", e)
+    input("Press enter to exit...")
+    sys.exit(1)
+
+try:
+    res = r.json()
+except json.JSONDecodeError as e:
+    print("Invalid response from server: ", e)
+    input("Press enter to exit...")
+    sys.exit(1)
+
+if res["code"] == 200:
+    print("Success!")
+    print("Updating cache...")
+    r = requests.get(update_endpoint)
+    try:
+        res = r.json()
+        if res["code"] != 200:
+            print("Server returned an error:  ", res)
+            input("Press enter to exit...")
+            sys.exit(1)
+        else:
+            print("Success")
+            print("Rock and stone!")
+            input("Press enter to exit...")
+    except json.JSONDecodeError as e:
+        print("Invalid response from server: ", e)
+        input("Press enter to exit...")
+        sys.exit(1)
+else:
+    print("Server returned an error:  ", res)
+    input("Press enter to exit...")
+    sys.exit(1)
